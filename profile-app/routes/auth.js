412298da3060f09570c94b9dcfef0d9c50283907
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-09-28 11:45:04 -0400

    Added login route

--- profile-app/routes/auth.js
+++ profile-app/routes/auth.js
@@ -12,12 +12,34 @@ router.get("/login", (req, res, next) => {
   res.render("auth/login", { "message": req.flash("error") });
 });
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, theUser, failureDetails) => {
+        if (err) {
+            res.status(500)
+                .json({ message: 'Something went wrong authenticating user' });
+
+            return;
+        }
+
+        if (!theUser) {
+            res.status(401)
+                .json(failureDetails);
+            return;
+        }
+
+        req.login(theUser, (err) => {
+            if (err) {
+                res.status(500)
+                    .json({ message: 'Session save went bad.' });
+
+                return;
+            }
+
+            res.status(200)
+                .json(theUser);
+        });
+    })(req, res, next);
+});
 
 router.get("/signup", (req, res, next) => {
   res.render("auth/signup");
