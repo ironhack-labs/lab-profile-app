750b64f078d2674c8e7a98c96793845e2ec4a65a
AdrianMelGon <adrianmelgon@gmail.com>
2018-10-09 11:11:44 +0200

    done

--- assets/profile-front/src/App.js
+++ assets/profile-front/src/App.js
@@ -77,34 +77,3 @@ class App extends Component {
 
 export default App;
 
-function checking (s, caps) {
-  let s1 = s.split("");
-  let caps1 = caps.split(""); 
-  let res = []
-  caps1.forEach((e, idx) => {
-    if (idx % 2 == 0)
-    {for (let i = 0; i < s1.length; i++) {
-      if (idx = s1[i]) {
-        res.push(true)
-        break
-      }
-    }
-  }  
-    else if (idx % 2 != 0) 
-    { let s2 = s1.reverse();
-     {for (let i = 0; i < s2.length; i++) {
-      if (idx = s2[i]) {
-        res.push(false)
-        break;
-      }
-    }
-  }}
-    
- })
-console.log(s1)
-console.log(caps1)
-
- return res[res.length -1]
-}
-
-checking("(Sensei [says) no!]", "()[]")
