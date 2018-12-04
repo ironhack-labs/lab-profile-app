5cb1c8019e1b6661bf19df3e58598889172f2419
Vermont <vermont.ironhack@gmail.com>
2018-12-04 09:37:02 -0600

    Styles and Profile changes

--- profile-react-app/src/Components/Profile/Profile.js
+++ profile-react-app/src/Components/Profile/Profile.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
+import LinkButton from '../Commun/LinkButton';
 
 class Profile extends Component {
   constructor(){
@@ -23,35 +25,39 @@ class Profile extends Component {
     //console.log(this.state.user)
     const {username, campus, course, profilePicture} = this.state.user;
     return (
-      <div>
-        <h1>Profile</h1>
-        <div>
-          <div>
-            <label>
-              Username
-              <p>{username}</p>
-            </label>
-            <label>
-              Campus
-              <p>{campus}</p>
-            </label>
-            <label>
-              Course
-              <p>{course}</p>
-            </label>
+      <div className='main-card'>
+        <div className='card-left'>
+          <div className='profile-data-field'>
+            <h1>Profile</h1>
           </div>
           <div>
+            <label className='label profile-data-field'>
+              Username
+              <p className='profile-data'>{username}</p>
+            </label>
+            <label className='label profile-data-field'>
+              Campus
+              <p className='profile-data'>{campus}</p>
+            </label>
+            <label className='label profile-data-field'>
+              Course
+              <p className='profile-data'>{course}</p>
+            </label>
+          </div>
+          <div className='profile-logout'>
             <p onClick={this.logout}>Logout</p>
           </div>
         </div>
-        <div>
-          <img src={profilePicture} alt={username} />
-          <input
-            type='file'
-            name='profilePicture' />
-
-            <p>The user is able to upload a new<br/> profile photo using NodeJS and<br/> Multer uploader</p>
-
+        <div className='card-rigth'>
+          <div className='picture-box'>
+            <img className='profile-picture' src={profilePicture} alt={username} />
+            <Link to='' className='button main-card-link'>
+              <LinkButton className='button' name='Edit Photo' />
+            </Link>
+          </div>
+          <div>
+            <p className='small-text'>The user is able to upload a new<br/> profile photo using NodeJS and<br/> Multer uploader</p>
+          </div>
         </div>
       </div>
     )
