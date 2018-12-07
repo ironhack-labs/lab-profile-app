4fe3479337bf93645f754f59e400cad5bb802d2d
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-12-07 16:27:37 +0100

    Iteration 1

---
+++ server/routes/auth.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const passport = require('passport');
+const router = express.Router();
+const User = require("../models/User");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+router.post('/auth/login', function (req, res, next) {
+  passport.authenticate('local', function (err, user, info) {
+    if (err) { return next(err); }
+    if (!user) { return res.status(500).json({ message: "User not found" }); }
+    req.logIn(user, function (err) {
+      if (err) { return next(err); }
+      return res.status(200).json(user)
+    });
+  })(req, res, next);
+});
+
+router.post('/auth/signup', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+    res.status(400).json({ message: 'Provide username and password' });
+    return;
+  }
+
+  User.findOne({ username }, (err, foundUser) => {
+
+    if (err) {
+      res.status(500).json({ message: "Username check went bad." });
+      return;
+    }
+
+    if (foundUser) {
+      res.status(400).json({ message: 'Username taken. Choose another one.' });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username: username,
+      password: hashPass
+    });
+
+    newUser.save(err => {
+      if (err) {
+        res.status(400).json({ message: 'Saving user to database went wrong.' });
+        return;
+      }
+
+      req.login(newUser, (err) => {
+        if (err) {
+          res.status(500).json({ message: 'Login after signup went bad.' });
+          return;
+        }
+        res.status(200).json(newUser);
+      });
+    });
+  });
+});
+
+router.post('/auth/upload', (req, res, next) => {
+  
+});
+
+router.put('/auth/edit', (req, res, next) => {
+  
+});
+
+router.get('/auth/loggedin', (req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  res.status(403).json({ message: 'Unauthorized' });
+});
+
+router.get('/auth/logout', (req, res, next) => {
+  req.logout();
+  res.status(200).json({ message: 'Log out success!' });
+});
+
+module.exports = router;
