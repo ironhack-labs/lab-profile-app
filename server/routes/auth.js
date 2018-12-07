77158eb513f52d0ef73eccf6254a249c16df0f67
PomPom248 <sofiroggero@gmail.com>
2018-12-07 16:48:06 +0100

    iteration 1

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,8 @@ const express = require("express");
 const passport = require('passport');
 const router = express.Router();
 const User = require("../models/User");
+const parser = require("../configs/cloudinary")
+
 // Bcrypt to encrypt passwords
 
 const bcrypt = require("bcrypt");
@@ -28,14 +30,13 @@ router.post('/login', (req, res, next) => {
 });
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password, campus, course } = req.body
   if (username === "" || password === "") {
     res.status(500).json({ message: "Indicate username and password" });
     return;
   }
 
-  User.findOne({ username },"username", (err, user) => {
+  User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
       res.status(500).json({ message: "The username already exists" });
       return;
@@ -46,7 +47,9 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      campus,
+      course
     });
 
     newUser.save(err => {
@@ -65,7 +68,36 @@ router.post("/signup", (req, res, next) => {
   });
 });
 
-router.post("/logout", (req, res, next) => {
+router.post("/upload", parser.single("image"), (req, res, next) => {
+  User.findOneAndUpdate({},
+    { image: req.file.url })
+    .then(() => {
+      res.status(200).json({
+        success: true,
+        image: req.file.url
+      })
+    })
+})
+
+router.post('/edit', (req, res) => {
+
+  User.findByIdAndUpdate(req.user.id, {
+    username: req.body.username,
+    campus: req.body.campus,
+    course: req.body.course
+  }, { new: true })
+  .then((user) => {
+      res.status(200).json(user);
+    });
+  // for (key in update) {
+  //   if (update[key] == '') {
+  //     delete update[key]
+  //   }
+  // }
+    
+});
+
+router.get("/logout", (req, res, next) => {
   req.logout();
   res.status(200).json({ message: 'Log out success!' });
 })
