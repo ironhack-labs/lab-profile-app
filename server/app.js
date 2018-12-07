bc12886556c5d634657f3d1c499b36060b3b03ba
Patricia LG <patri.lopg5@gmail.com>
2018-12-07 14:12:21 +0100

    structure done and rute

---
+++ server/app.js
@@ -0,0 +1,63 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+const session    = require("express-session");
+const MongoStore = require('connect-mongo')(session);
+const flash      = require("connect-flash");
+
+
+mongoose
+  .connect(process.env.DB, {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+
+// Enable authentication using session + passport
+app.use(session({
+  secret: 'irongenerator',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+app.use(flash());
+require('./passport')(app);
+    
+
+const index = require('./routes/index');
+app.use('/', index);
+
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
+      
+// const authRoutes = require('./routes/auth-routes');
+// app.use('/api', authRoutes);
+// ...
+
+module.exports = app;
---
+++ server/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: String,
+  password: String,
+  campus : String, enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "México", "Sao Paulo"],
+  course : String, enum: ["WebDev", "UX/UI", "Data Analytics"],
+  image : String
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
