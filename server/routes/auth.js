251b2612b2c28a4848942a3a944bad4f090d79ee
Patricia LG <patri.lopg5@gmail.com>
2018-12-07 16:23:31 +0100

    iteration 1 done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 
@@ -7,26 +7,34 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
-
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
+router.post("/login", function(req, res, next) {
+  passport.authenticate("local", function(err, user, info) {
+    if (err) {
+      return res.status(200).json({ message: "login error" });
+    }
+    if (!user) {
+      return res.status(400).json({ message: "user no exists" });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.status(200).json(user);
+    });
+  })(req, res, next);
+});
 
 router.post("/signup", (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
   if (username === "" || password === "") {
-    res.status(200).json({ message: "Indicate username and password" });
+    res.status(500).json({ message: "Indicate username and password" });
     return;
   }
 
   User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
-      res.status(400).json( { message: "The username already exists" });
+      res.status(400).json({ message: "The username already exists" });
       return;
     }
 
@@ -38,19 +46,34 @@ router.post("/signup", (req, res, next) => {
       password: hashPass
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.status(400).json("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+      .then(user => {
+        res.status(200).json(user);
+      })
+      .catch(err => {
+        res.status(400).json({ message: "Something went wrong" });
+      });
   });
 });
 
+
+
+router.get('/loggedin', (req, res, next) => {
+  // req.isAuthenticated() is defined by passport
+  if (req.isAuthenticated()) {
+      res.status(200).json({ message: "User loggedin" });
+      return;
+  }
+  res.status(403).json({ message: 'Unauthorized' });
+});
+
+
 router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect("/");
+  res.status(200).json({ message: "user logout" });
+
 });
 
 module.exports = router;
+
