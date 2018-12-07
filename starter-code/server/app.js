02664388dcc422ae15c6f52bd67cd4a93190cc75
cocobilbao <cocobilbao@gmail.com>
2018-12-07 17:33:08 +0100

    iteration 1

---
+++ starter-code/server/app.js
@@ -0,0 +1,81 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const favicon      = require('serve-favicon');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+const session    = require("express-session");
+const MongoStore = require('connect-mongo')(session);
+
+const passport      = require('passport');
+
+const cors = require('cors');
+  
+const routes = require('./routes/auth');
+app.use('/api', routes);
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000']
+}));
+
+mongoose
+  .connect('mongodb://localhost/server', {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(session({
+  secret:"some secret goes here",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+      
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+// Enable authentication using session + passport
+app.use(session({
+  secret: 'irongenerator',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+
+
+const index = require('./routes/index');
+app.use('/', index);
+
+const routes = require('./routes/auth');
+app.use('/auth', routes);
+      
+
+module.exports = app;
