146ed28ce836bd41385d17bf5d02e66c72c430bd
Bianca Castro <biiianca_00@hotmail.com>
2018-12-07 15:15:50 +0100

    create model and import packeages

---
+++ server/app.js
@@ -0,0 +1,59 @@
+require('dotenv').config();
+
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express      = require('express');
+const mongoose     = require('mongoose');
+const logger       = require('morgan');
+const path         = require('path');
+
+const session    = require("express-session");
+const MongoStore = require('connect-mongo')(session);
+const flash      = require("connect-flash");
+    
+
+mongoose
+  .connect(process.env.DBURL, {useNewUrlParser: true})
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    console.error('Error connecting to mongo', err)
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Enable authentication using session + passport
+app.use(session({
+  secret: 'irongenerator',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+app.use(flash());
+require('./passport')(app);
+    
+
+const index = require('./routes/index');
+app.use('/', index);
+
+const authRoutes = require('./routes/auth');
+app.use('/auth', authRoutes);
+      
+
+module.exports = app;
