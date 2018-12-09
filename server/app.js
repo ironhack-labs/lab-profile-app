a3d3dff7a6ea3d077a7354782bdfcd1c131f983d
SandraLuengo <you@example.com>
2018-12-09 13:15:46 +0100

    Iteracion 1, sin las rutas de subida de imagenes

---
+++ server/app.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const cors = require('cors');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+
+mongoose
+  .connect('mongodb://localhost/server', { useNewUrlParser: true })
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err);
+  });
+
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+
+// Enable authentication using session + passport
+app.use(session({
+  secret: 'irongenerator',
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+}));
+
+require('./passport')(app);
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'],
+}));
+
+
+app.use('/', require('./routes/index'));
+
+
+module.exports = app;
---
+++ server/routes/auth.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const passport = require('passport');
+
+const authRoutes = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+// Bcrypt to encrypt passwords
+const bcryptSalt = 10;
+
+authRoutes.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, failureDetails) => {
+    if (err) {
+      res.status(500).json({ message: 'Error in the authentication' });
+      return;
+    }
+    if (!user) {
+      res.status(500).json(failureDetails);
+      return;
+    }
+
+    req.login(user, (error) => {
+      if (error) {
+        res.status(500).json({ message: 'Error in the authentication' });
+        return;
+      }
+      res.status(200).json(user);
+    });
+  })(req, res, next);
+});
+
+authRoutes.post('/signup', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === '' || password === '') {
+    res.status(500).json({ message: 'Provide username and password' });
+    return;
+  }
+
+  User.findOne({
+    username,
+  }, 'username', (err, user) => {
+    if (user !== null) {
+      res.status(500).json({ message: 'Username taken' });
+      return;
+    }
+
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User({
+      username,
+      password: hashPass,
+    });
+
+    newUser.save((er) => {
+      if (er) {
+        res.status(500).json({ message: 'Saving user to database went wrong.' });
+      } else {
+        req.login(newUser, (e) => {
+          if (e) {
+            res.status(500).json({ message: 'Login after signup went bad.' });
+            return;
+          }
+          res.status(200).json(newUser);
+        });
+      }
+    });
+  });
+});
+
+authRoutes.get('/logout', (req, res) => {
+  req.logout();
+  res.status(200).json({
+    message: 'Log out success!',
+  });
+});
+
+authRoutes.get('/loggedin', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json(req.user);
+    return;
+  }
+  res.status(403).json({ message: 'Unauthorized' });
+});
+
+// Edicion del perfil y subida de imagenes
+
+// authRoutes.post('/upload', (req, res) => {
+
+// });
+
+// authRoutes.post('/edit', (req, res) => {
+
+// });
+
+module.exports = authRoutes;
