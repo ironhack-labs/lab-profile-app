e38abea974ee72f89552d3c3e00549c13388bc1c
Teresa Romero <tromerolozano@gmail.com>
2019-01-25 08:59:17 +0100

    REST API auth

--- server/models/User.js
+++ server/models/User.js
@@ -1,15 +1,33 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: String,
-  password: String
-}, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
+const userSchema = new Schema(
+  {
+    username: String,
+    password: String,
+    campus: {
+      type: String,
+      enum: [
+        "Madrid",
+        "Barcelona",
+        "Miami",
+        "Paris",
+        "Berlin",
+        "Amsterdam",
+        "Mexico",
+        "Sao Paulo"
+      ]
+    },
+    course: { type: String, enum: ["WebDev", "UX/UI", "Data Analytics"] },
+    image: String
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
   }
-});
+);
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 module.exports = User;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const passport = require('passport');
+const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User");
 
@@ -7,33 +7,33 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+let loginPromise = (req, user) => {
+  return new Promise((resolve, reject) => {
+    req.login(user, e => (e ? reject(e) : resolve(user)));
+  });
+};
 
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
-});
-
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
-});
+router.post(
+  "/login",
+  passport.authenticate("local", (err, user, failureDetails) => {
+    if (err) return res.status(500).json({ message: "Something went wrong" });
+    if (!user) return res.status(401).json(failureDetails);
+    loginPromise(req, user)
+      .then(() => res.status(200).json(req.user))
+      .catch(e => res.status(500).json({ message: e.message }));
+  })(req, res, next)
+);
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
+  const { username, password, campus, course } = req.body;
+  if (username === "" || password === "" || campus === "" || campus === "") {
+    res.json({ message: "Indicate all fields" });
     return;
   }
 
   User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
+      res.json({ message: "The username already exists" });
       return;
     }
 
@@ -42,22 +42,39 @@ router.post("/signup", (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass
+      password: hashPass,
+      campus,
+      course
     });
 
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
-    })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    newUser
+      .save()
+      .then(user => loginPromise(req, user))
+      .catch(err => {
+        res.json({ message: "Something went wrong" });
+      });
   });
+});
+
+router.get("edit", (req, res, next) => {
+  const { username, campus, course, id } = req.body;
+
+  User.findByIdAndUpdate(id, { username, campus, course }).then(userUpdat =>
+    res.json({ message: "Updated Success", userUpdat })
+  );
+});
+
+router.get("loggedin", (req, res) => {
+  const { user } = req;
+
+  user
+    ? res.json({ succes: "Ok", user })
+    : res.status(401).json({ succes: "No user logged" });
 });
 
 router.get("/logout", (req, res) => {
   req.logout();
-  res.redirect("/");
+  res.json({ success: "Ok" });
 });
 
 module.exports = router;
