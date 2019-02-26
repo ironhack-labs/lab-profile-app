41196b2fe266a7addb86dae043dd180a39210ad2
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-02-26 16:26:40 +0100

    profile page code improved

--- front/src/Components/Auth/profile.js
+++ front/src/Components/Auth/profile.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 const Profile = ({ user }) => {
-  return (
-    <Route
-
-      render={props => {
-        if (user) {
-          return (
-            <div>
-              <h3>Welcome back, {user.username}!!!</h3>
-              <ul>
-                <li>Campus: {user.campus}</li>
-                <li>Course: {user.course}</li>
-              </ul>
-            </div>
-          )
-        }
-        else return <Redirect to={{ pathname: '/' }} />
-      }}
-    />
-  )
+  if (user) {
+    return (
+      <div>
+        <h3>Welcome back, {user.username}!!!</h3>
+        <ul>
+          <li>Campus: {user.campus}</li>
+          <li>Course: {user.course}</li>
+        </ul>
+      </div>
+    )
+  }
+  else return <Redirect to={{ pathname: '/' }} />
 }
 
 export default Profile;
