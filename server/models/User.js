b1053dc7e6325fad99c9694b15db1eb5cfe37fc8
Raúl Contreras <raulc98@gmail.com>
2019-05-06 22:42:29 +0200

    Created routes

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,16 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  campus: {
+    type: String,
+    enum: ['Madrid', 'Barcelona', 'Miami', 'Paris', 'Berlin', 'Amsterdam', 'México', 'Sao Paulo']
+  },
+  course: {
+    type: String,
+    enum: ['WebDev', 'UX/UI', 'Data Analytics']
+  },
+  image: String,
 }, {
   timestamps: {
     createdAt: 'created_at',
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,57 +7,97 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
+const login = (req, user) => {
+  return new Promise((resolve, reject) => {
+    req.login(user, err => {
+      if(err){
+        reject(new Error('Something went wrong'));
+      }else{
+        resolve(user);
+      } 
+    })
+  })
+}
 
-router.get("/login", (req, res, next) => {
-  res.render("auth/login", { "message": req.flash("error") });
-});
+router.post("/login", (req, res, next) => {
+  passport.authenticate('local', (err, theUser, failureDetails) => {
+    if(err){
+      next(new Error('Something went wrong'));
+    }
+    if(!theUser){
+      next(failureDetails);
+    }
 
-router.post("/login", passport.authenticate("local", {
-  successRedirect: "/",
-  failureRedirect: "/auth/login",
-  failureFlash: true,
-  passReqToCallback: true
-}));
-
-router.get("/signup", (req, res, next) => {
-  res.render("auth/signup");
+    login(req, theUser)
+      .then(user => res.status(200).json(req.user));
+  })(req, res, next);
 });
 
 router.post("/signup", (req, res, next) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if (username === "" || password === "") {
-    res.render("auth/signup", { message: "Indicate username and password" });
-    return;
+  const {username, password, campus, course} = req.body;
+
+  if(username.trim().length <= 0 || password.trim().length <= 0 || campus.trim().length <= 0 || course.trim().length <= 0){
+    res.status(500).json({
+      code: 500,
+      message: 'You must provide valid credentials'
+    })
   }
 
-  User.findOne({ username }, "username", (err, user) => {
-    if (user !== null) {
-      res.render("auth/signup", { message: "The username already exists" });
-      return;
-    }
+  User.findOne({username})
+    .then(foundUser => {
+      if(foundUser){
+        throw new Error('Username already exists');
+      }
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
+      const salt = bcrypt.genSaltSync(10);
+      const hashPass = bcrypt.hashSync(password, salt);
 
-    const newUser = new User({
-      username,
-      password: hashPass
-    });
-
-    newUser.save()
-    .then(() => {
-      res.redirect("/");
+      return new User({
+        username,
+        password: hashPass,
+        campus,
+        course
+      }).save();
     })
-    .catch(err => {
-      res.render("auth/signup", { message: "Something went wrong" });
-    })
+    .then(savedUser => login(req, savedUser))
+    .then(user => res.json({
+      code: 200, 
+      message: user
+    }))
+    .catch(err => res.status(500).json({
+      code: 500,
+      message: err
+    }));
+});
+
+router.post('/upload', (req, res, next)=>{
+
+});
+
+router.post('/edit', (req, res, next)=>{
+
+});
+
+router.get('/logout', (req, res, next) => {
+  req.logout();
+  res.status(200).json({
+    code: 200,
+    message: 'logged out'
   });
 });
 
-router.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/");
+router.get('/loggedin', (req, res, next) => {
+  if(req.user){
+    res.status(200).json({
+      code: 200,
+      message: req.user
+    });
+  }else{
+    res.status(401).json({
+      code: 401,
+      message: 'User not logged'
+    })
+  }
 });
 
 module.exports = router;
