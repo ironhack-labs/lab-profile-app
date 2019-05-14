a2d69e13e87ff23663daa126ff316f0905aa2826
brennedith <brennedithg@gmail.com>
2019-05-13 22:15:10 -0500

    Work on login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import CenteredBox from './CenteredBox';
+
+class Login extends Component {
+  state = {
+    name: '',
+    password: '',
+    message: null
+  };
+
+  handleInput = ({ target: input }) => {
+    const { name, value } = input;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = e => {
+    if (e) e.preventDefault();
+
+    console.log(this.state);
+  };
+
+  submitForm = () => {
+    this.handleSubmit();
+  };
+
+  render() {
+    const { name, password, message } = this.state;
+
+    const leftPanel = (
+      <>
+        <h1 className="title">Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {message && <p className="notification is-danger">Error Message</p>}
+          <label className="label">
+            Name
+            <input
+              type="text"
+              name="name"
+              value={name}
+              className="input"
+              onChange={this.handleInput}
+            />
+          </label>
+          <label className="label">
+            Password
+            <input
+              type="password"
+              name="password"
+              value={password}
+              className="input"
+              onChange={this.handleInput}
+            />
+          </label>
+          <button type="submit" hidden />
+        </form>
+        <p>
+          If you don't have an account yet, you can create your account{' '}
+          <Link to="/signup">here</Link>.
+        </p>
+      </>
+    );
+
+    const rightPanel = (
+      <>
+        <div>
+          <h1 className="title">Hello</h1>
+          <h2 className="subtitle">
+            Awesome to have you at IronProfile again!
+          </h2>
+        </div>
+        <div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus quis maiores enim consequatur? Veniam ea sint
+            perspiciatis quae ad iusto?
+          </p>
+          <br />
+          <button className="button is-block is-dark" onClick={this.submitForm}>
+            Log In
+          </button>
+        </div>
+      </>
+    );
+
+    return <CenteredBox leftPanel={leftPanel} rightPanel={rightPanel} />;
+  }
+}
+
+export default Login;
