acd67c1f76629be2b9ee5852ab0632a6f2fc265a
brennedith <brennedithg@gmail.com>
2019-05-14 09:08:11 -0500

    Work on profile

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
 import CenteredBox from './CenteredBox';
 
+import AuthService from '../helpers/AuthService';
+
 class Profile extends Component {
+  state = {
+    user: null,
+    message: null
+  };
+
+  componentDidMount() {
+    AuthService.loggedin()
+      .then(({ data: user }) => {
+        this.setState({ user });
+      })
+      .catch(({ response: { data } }) => {
+        this.setState({ message: data.message });
+      });
+  }
+
+  handleLogout = () => {
+    AuthService.logout();
+    this.props.history.push('/');
+  };
+
   render() {
+    const { user, message } = this.state;
+
+    if (!user) return <p className="notification is-danger">{message}</p>;
+
     const leftPanel = (
       <>
         <h1 className="title">Profile</h1>
         <div>
           <p className="subtitle is-6">Username</p>
-          <p className="title is-5">Username</p>
+          <p className="title is-5">{user.username}</p>
 
           <p className="subtitle is-6">Campus</p>
-          <p className="title is-5">Campus</p>
+          <p className="title is-5">{user.campus}</p>
 
           <p className="subtitle is-6">Course</p>
-          <p className="title is-5">Course</p>
+          <p className="title is-5">{user.course}</p>
         </div>
         <p>
-          <Link to="/signout" className="has-text-danger">
+          <button className="has-text-danger" onClick={this.handleLogout}>
             Logout
-          </Link>
+          </button>
         </p>
       </>
     );
