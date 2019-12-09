17d7aced2e490bc9630b870480123fc376ab4a45
EChColorado <edxz7c@gmail.com>
2019-12-09 02:02:14 -0600

    Fail in logout and it redirect incorrectly on create user, and the profile name are wrongly display

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const {model, Schema} = require('mongoose');
+const PLM = require("passport-local-mongoose");
+const userSchema = new Schema(
+    {
+        username:String,
+        password:String,
+        campus: {
+            type:String,
+            enum:['Madrid','Barcelona','Miami','Paris', 'Berlin', 'Amsterdam', 'México', 'Sao Paulo', 'Lisbon']
+        },
+        course:{
+            type:String,
+            enum:['WebDev', 'UX/UI', 'Data Analytics']
+        },
+        image:{
+            type:String
+        }
+    }
+)
+userSchema.plugin(PLM, {
+    usernameField : 'username'
+  });
+module.exports = model('User', userSchema);
+
