bb6276b909262a63ee83dd5d8b60493049ed2d26
EChColorado <edxz7c@gmail.com>
2019-12-08 19:29:00 -0600

    Iteration 3 60%

---
+++ front/src/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import LogIn from "./components/LogIn";
+import NotFound from "./components/404/NotFound.js";
+
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={LogIn} />
+      <Route exact path="/signup" component={SignUp} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
