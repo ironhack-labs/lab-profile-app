d0134a39c7f04946789c8549b5d870add77a8081
Carlos Dominguez <carlos.cfdr86@gmail.com>
2020-02-22 21:53:45 -0600

    Iteration 2 - The React App

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container">
+      <section className="leftContent ">
+        <h1>IronProfile</h1>
+        <p>Today we will create an app authoritation, adding some cool styles!</p>
+        <Link to="/signup">Sign up</Link>
+        <Link to="/login">Log in</Link>
+      </section>
     </div>
   );
 }
