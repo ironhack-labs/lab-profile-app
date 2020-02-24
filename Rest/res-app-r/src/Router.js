c52bdd9d7aba6e6c9a55d257a3eb15da8c30d4e9
israSH <isra-03p@hotmail.com>
2020-02-23 21:15:21 -0600

    Botones signupy login

--- Rest/res-app-r/src/Router.js
+++ Rest/res-app-r/src/Router.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 //import Navbar from './components/Navbar'
-//import Home from './pages/home'
+import Home from './pages/home'
 //import Login from './pages/login'
 //import Signup from './pages/signup'
 //import Profile from './pages/profile'
 const Profile = () => <h1>profile</h1>;
 const Signup = () => <h1>signup</h1>;
 const Login = () => <h1>login</h1>
-const Home = () => <h1>Home</h1>
+//const Home = () => <h1>Home</h1>
 
 //<Navbar />
 export const Router = () => (
---
+++ Rest/res-app-r/src/pages/home/index.js
@@ -0,0 +1,15 @@
+ 
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <h1>Home</h1>
+                <Link to='/signup'><button>Sign up</button></Link>
+                <Link to='/login'><button>Login</button></Link>
+            </div>
+        )
+    }
+}
