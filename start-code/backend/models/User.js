a4dc9d0114c35cb4061be9ff4a885c47395bec2b
Macbook pro <macbookpro@Macbooks-MacBook-Pro.local>
2020-02-26 23:53:54 -0600

    final

---
+++ start-code/backend/models/User.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+const PLM = require('passport-local-mongoose');
+
+const userSchema = new Schema(
+  {
+    email: String,
+    name: String,
+    campus: String,
+    enum: 
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+userSchema.plugin(PLM, { usernameField: 'email' });
+
+module.exports = model('User', userSchema);
