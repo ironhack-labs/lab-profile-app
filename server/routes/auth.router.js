4ab35960fe101cff37e1e1b98054712c5c743824
Ana Segarra <anasegarraber@gmail.com>
2020-03-24 13:40:20 +0100

    Handle validation errors in signup

--- server/routes/auth.router.js
+++ server/routes/auth.router.js
@@ -5,21 +5,32 @@ const User = require('../models/User');
 
 // POST route - create new user
 router.post('/signup', async (req, res, next) => {
-  const { username, password } = req.body;
+  const { username, password, campus, course } = req.body;
   try {
-    const newUser = await User.create({ username, password });
+    const newUser = await User.create({ username, password, campus, course });
     console.log('Created user ', newUser);
     return res.status(201).json({
       status: 201,
       message: 'User registered successfully '
     });
   } catch (error) {
-    console.log('Error occurred during signup', error);
-    res.status(500).json({
-      status: 500,
-      message: 'Signup failed'
-    });
+    if (error.name === 'ValidationError') {
+      console.log('Validation error ', error);
+      return res.status(400).json({
+        status: 400,
+        message: error.message
+      });
+    } else {
+      console.log('Error occurred during signup', error);
+      return res.status(500).json({
+        status: 500,
+        message: 'Signup failed'
+      });
+    }
   }
 });
+
+// POST route - create logged user
+router.post('/login', async (req, res, next) => {});
 
 module.exports = router;
