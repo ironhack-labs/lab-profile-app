c41d0302a7d3f9669f9f5de153b7d29ebb9a4649
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-03-25 17:54:54 +0100

    Create /login route

--- starter-code/server/routes/auth.js
+++ starter-code/server/routes/auth.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const _ = require('lodash');
+const ensureLogin = require('connect-ensure-login');
 const User = require('../models/User');
+
+router.post(
+  '/login',
+  ensureLogin.ensureLoggedOut(),
+  passport.authenticate('local'),
+  (req, res) => {
+    // Return the logged in user
+    return res.json(
+      _.pick(req.user, ['username', '_id', 'createdAt', 'updatedAt'])
+    );
+  }
+);
 
 module.exports = router;
