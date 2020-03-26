0cdf12a8fa7e85a3877888c9da1e08a831cc86ad
Ana Segarra <anasegarraber@gmail.com>
2020-03-26 17:20:09 +0100

    Set up cors

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ const logger = require('morgan');
 const path = require('path');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const cors = require('cors');
 
 mongoose
   .connect('mongodb://localhost/server', {
@@ -32,7 +33,23 @@ const debug = require('debug')(
 
 const app = express();
 
+// Cross Domain CORS setup
+const whitelist = ['http://localhost:3000', 'http://localhost:1234'];
+const corsOptions = {
+  origin: function(origin, callback) {
+    console.log(`Origin: ${origin}`);
+    if (!origin) return callback(null, true);
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+};
+
 // Middleware Setup
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
