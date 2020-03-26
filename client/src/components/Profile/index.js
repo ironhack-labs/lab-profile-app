974177e99687e95635b990c875b763d9f4d74231
vctorChamizo <victor.chamizo96@gmail.com>
2020-03-26 13:04:07 +0100

    Add profile component

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -6,6 +6,7 @@ import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
 
 import { InfoUser } from "../Profile/infoUser";
 
@@ -15,21 +16,42 @@ const test = {
   course: "DevWeb"
 };
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     justifyContent: "center"
   },
   card: {
     width: "50%",
-    height: "60vh",
+    height: "65vh",
     padding: "5vh 5vw",
-    margin: "15vh 0",
+    margin: "10vh 0",
     border: "1px solid #5c6bc0"
   },
   wrapperButton: {
     display: "flex",
     justifyContent: "center"
+  },
+  wrapperInfo: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "flex-start"
+  },
+  wrapperImage: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  large: {
+    margin: "0 5vw",
+    width: theme.spacing(25),
+    height: theme.spacing(25),
+    fontSize: theme.spacing(10)
+  },
+  button: {
+    marginTop: "2.5vh",
+    border: "1px solid #777",
+    padding: "0 2.5vw"
   }
 }));
 
@@ -41,21 +63,30 @@ export const Profile = () => {
   return (
     <Container className={classes.root} component="main">
       <Card className={classes.card}>
-        <Typography
-          variant="h4"
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
+        <Typography variant="h3" color="textSecondary" gutterBottom>
           Profile
         </Typography>
 
-        {Object.entries(user).map(([key, value]) => (
-          <InfoUser key={key} title={key.toUpperCase()} value={value} />
-        ))}
+        <div className={classes.wrapperInfo}>
+          <div>
+            {Object.entries(user).map(([key, value]) => (
+              <InfoUser key={key} title={key.toUpperCase()} value={value} />
+            ))}
+          </div>
+          <div className={classes.wrapperImage}>
+            <Avatar
+              alt="Avatar Image"
+              src={user.image || ""}
+              className={classes.large}
+            >
+              {user.username[0].toLocaleUpperCase()}
+            </Avatar>
+            <Button className={classes.button}>Edit Photo</Button>
+          </div>
+        </div>
 
         <CardActions className={classes.wrapperButton}>
-          <Button>Log Out</Button>
+          <Button className={classes.button}>Log Out</Button>
         </CardActions>
       </Card>
     </Container>
