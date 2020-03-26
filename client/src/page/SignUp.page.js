67405045e9577ac6d63389054284347fa2a1c764
David Garcia <dgarciabravo13@gmail.com>
2020-03-26 20:55:44 +0100

    interation1 done

---
+++ client/src/page/SignUp.page.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Col2 } from "../../public/styles/Common.styles";
+
+export const SignUpPage = () => {
+  return (
+    <Col2>
+      <div className="left">
+        <h1>Registro</h1>
+        <label htmlFor="">Email de Usuario</label>
+        {/* <input
+          type="email"
+          id="username"
+          placeholder="email@email.com"
+          value=""
+          onChange=""
+        /> */}
+      </div>
+      <div className="right">
+        <h2>
+          ¡Hola! <span>Bienvenido a tu IronPerfil</span>
+        </h2>
+        <p>
+          Si realizas el registro, estás aceptando los términos y condiciones
+          donde nosotros podemos hacer lo que queramos con sus datos.
+        </p>
+        <button className="button">Crea tu cuenta</button>
+      </div>
+    </Col2>
+  );
+};
