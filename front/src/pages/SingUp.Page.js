de707b630822e0674a628edf166879a1f32979eb
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-03-26 22:02:00 +0100

    iteration4 sing up

--- front/src/pages/SingUp.Page.js
+++ front/src/pages/SingUp.Page.js
@@ -1,5 +1,34 @@
 import React from "React";
+import { useForm } from "react-hook-form";
 
 export const SingUp = () => {
-  return <div></div>;
+  const { register, handledSubmit, errors } = useForm({ mode: onblur });
+  const Onsubmit = data => {
+       console.log(data);
+  };
+  return(
+<>
+<h1>Sing Up</h1>
+<form Onsubmit={handledSubmit}>
+        <div>
+          <label>Username</label>
+          <Input
+            className={hasError(errors, "username")}
+            name="username"
+            ref={register({ required: true })}
+          />
+        </div>
+        <div>
+          <label>Password</label>
+          <Input
+            className={hasError(errors, "username")}
+            name="username"
+            ref={register({ required: true })}
+          />
+        </div>
+
+</form>
+<>
+
+  );
 };
