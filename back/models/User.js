5983dc5540cc4ac037b056a1f4977bb1648b52f9
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-03-26 14:08:20 +0100

    structure back

---
+++ back/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, unique: true, index: true },
+    password: String,
+    campus: String,
+    course: String,
+    image: String,
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Users = mongoose.model("Users", userSchema);
+
+Users.collection.createIndexes([
+  {
+    key: { username: 1 },
+    name: "username"
+  }
+]);
+
+module.exports = Users;
