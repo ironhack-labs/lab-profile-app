d9ce000768b7ddd2c06d1bb8c0b080ff4d6e4d8f
Ana Segarra <anasegarraber@gmail.com>
2020-03-26 02:54:13 +0100

    Fix routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 // dependencies
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home } from './pages/Home.component';
+import { Home } from './components/Home/Home.component';
 import { Layout } from './Layout/Layout.component';
+import { Signup } from './components/Signup/Signup.component';
 
 export const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route to="/" exact component={Home} />
-          <Route to="/signup" />
-          <Route to="/login" />
+          <Route path="/" exact component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" />
         </Switch>
       </Layout>
     </Router>
