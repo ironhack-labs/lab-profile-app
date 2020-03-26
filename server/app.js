9a77bfb7584becfdd6c3436be326d8fb2ed7fba3
Ana Segarra <anasegarraber@gmail.com>
2020-03-26 20:53:47 +0100

    Clean app.js file

--- server/app.js
+++ server/app.js
@@ -62,25 +62,10 @@ app.use(
     store: new MongoStore({ mongooseConnection: mongoose.connection })
   })
 );
+
 require('./config/passport')(app);
 
-// Express View engine setup
-app.use(
-  require('node-sass-middleware')({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    sourceMap: true
-  })
-);
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
+// routes
 const index = require('./routes/index');
 app.use('/', index);
 
