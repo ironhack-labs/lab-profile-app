c137b360ef89979ce2e1222ef3b9cbf7566db3da
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-03-27 21:22:15 +0100

    iteration4 done

--- front/src/app.js
+++ front/src/app.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./pages/Home.pages";
 import { LoginPage } from "./pages/Login.Page";
 import { SignUpPage } from "./pages/SignUp.Page";
+import { ProfilePage } from "./pages/Profile.pages";
+
 import { ApiContextProvider } from "../context/ApiContext";
 
 export const App = () => (
@@ -12,6 +14,7 @@ export const App = () => (
         <Route exact path="/" component={Home} />
         <Route path="/login" component={LoginPage} />
         <Route path="/signUp" component={SignUpPage} />
+        <Route path="/profile" component={ProfilePage} />
       </Switch>
     </ApiContextProvider>
   </Router>
