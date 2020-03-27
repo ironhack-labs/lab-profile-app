de8a93ba5c0327620b8b8e67f527d25bfd3696fb
Ivan Esteban <ivan.estebanisabel@gmail.com>
2020-03-27 20:49:38 +0100

    client fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-export const App = () => <div>start</div>;
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { UserContextProvider } from "./components/context/Context";
+import { HomePage } from "./pages/Home.Page";
+import { SignUpPage } from "./pages/SignUp.Page";
+import { LoginPage } from "./pages/Login.Page";
+import { ProfilePage } from "./pages/Profile.Page";
 
 export const App = () => (
   <Router>
     <UserContextProvider>
       <Container>
         <Content>
-          <div>Hola</div>;
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/signup" component={SignUpPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/profile" component={ProfilePage} />
+          </Switch>
         </Content>
       </Container>
     </UserContextProvider>
