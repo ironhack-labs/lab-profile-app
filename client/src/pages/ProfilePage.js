fba9101a2a5cd485fee450b85a9602c4e0f6a815
J. A. Hernández <joseanher81@gmail.com>
2020-03-27 21:25:42 +0100

    ProfilePage partially implemented

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { logout, upload } from '../services/authService';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import { withRouter } from "react-router-dom";
 
-const ProfilePage = () => {
+const ProfilePage = withRouter(({ history }) => {
+  const [file, setFile] = useState('');
+
+  const handleSubmit =  async (e) => {
+    e.preventDefault();
+    try {
+      const res = await upload({file})
+      console.log(res);
+      history.push("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logout = async (e) => {
+    try {
+      const res = await logout({username})
+      console.log(res);
+      history.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return(
-    <h1>Profile page!</h1>
+    <Container>
+      <h1>Profile</h1>
+      <p>Username</p>
+      <p>Username</p>
+      <p>Campus</p>
+      <p>Username</p>
+      <p>Course</p>
+      <p>Username</p>
+      <p><a href='' onClick={logout()}>Log out</a></p>
+
+      <Form onSubmit={handleSubmit} >
+            <div style={{textAlign: 'center'}}>
+              <Button variant="primary" type="submit" >
+                Edit Photo
+              </Button>
+            </div>
+          </Form>    
+    </Container>
   );
-}
+});
 
 export default ProfilePage;
--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -24,39 +24,39 @@ const SignupPage =  withRouter(({ history })=> {
 
   return(
     <Container>
-    <h1>Sign Up</h1>
-    <h2>Hello!!</h2>
-    <h3>Welcome to IronProfile!</h3>
-    <p>If you sign up, you agree with all our terms and conditions where we can do whatever we want with the data!</p>
+      <h1>Sign Up</h1>
+      <h2>Hello!!</h2>
+      <h3>Welcome to IronProfile!</h3>
+      <p>If you sign up, you agree with all our terms and conditions where we can do whatever we want with the data!</p>
 
-    <Form onSubmit={handleSubmit} >
-          <Form.Group controlId="username">
-            <Form.Label>Username</Form.Label>
-            <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-          </Form.Group>
+      <Form onSubmit={handleSubmit} >
+            <Form.Group controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </Form.Group>
 
-          <Form.Group controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-          </Form.Group>
+            <Form.Group controlId="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </Form.Group>
 
-          <Form.Group controlId="campus">
-            <Form.Label>Campus</Form.Label>
-            <Form.Control type="text" value={campus} onChange={(e) => setCampus(e.target.value)}/>
-          </Form.Group>
+            <Form.Group controlId="campus">
+              <Form.Label>Campus</Form.Label>
+              <Form.Control type="text" value={campus} onChange={(e) => setCampus(e.target.value)}/>
+            </Form.Group>
 
-          <Form.Group controlId="course">
-            <Form.Label>Course</Form.Label>
-            <Form.Control type="text" value={course} onChange={(e) => setCourse(e.target.value)}/>
-          </Form.Group>
+            <Form.Group controlId="course">
+              <Form.Label>Course</Form.Label>
+              <Form.Control type="text" value={course} onChange={(e) => setCourse(e.target.value)}/>
+            </Form.Group>
 
-          <div style={{textAlign: 'center'}}>
-            <Button variant="primary" type="submit" >
-              Create the Account
-            </Button>
-          </div>
-        </Form>    
-  </Container>
+            <div style={{textAlign: 'center'}}>
+              <Button variant="primary" type="submit" >
+                Create the Account
+              </Button>
+            </div>
+          </Form>    
+    </Container>
   );
 });
 
