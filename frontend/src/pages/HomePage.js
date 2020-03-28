13bff8ecf529fdbb0f21a1f9e6ca100d6b14b91d
Carlos Mateo <cmc2k5@gmail.com>
2020-03-29 00:32:14 +0100

    Front pages - Completed

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 29px;
+    color: #517052;
+    margin-top: 0;
+`;
+
+const Center = styled.div`
+    text-align: center;
+    margin-top: 100px;
+`;
+
+const Text = styled.p`
+    font-size: 20px;
+    color: #56585B;
+`;
+
+export const HomePage = () => {
+    return (
+        <div className="col-half">
+            <Title>IronProfile</Title>
+            <Text>Today we will create an app with authoritaton, adding some cool styles!</Text>
+            <Center>
+                <Link className="btn btn--primary" to="/">Sign up</Link>
+                <Link className="btn btn--primary" to="/">Log in</Link>
+            </Center>
+        </div>
+    )
+}
---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 29px;
+    color: #517052;
+    margin-top: 0;
+`;
+
+const ImgWrapper = styled.div`
+    background: #DADADA;
+    width: 180px;
+    height: 180px;
+    margin: auto;
+    border-radius: 50%;
+    overflow: hidden;
+    margin-bottom: 20px;
+`;
+
+const Small = styled.div`
+    max-width: 200px;
+    font-size: 12px;
+    color: #657865;
+    text-align: left;
+    margin-top: 60px;
+`;
+
+const Text = styled.p`
+    font-size: 20px;
+    color: #56585B;
+`;
+
+export const ProfilePage = () => {
+    return (
+        <>
+            <div className="col-left">
+                <Title>Profile</Title>
+                <dl>
+                    <dt>Username</dt>
+                    <dd>Harry Potter</dd>
+                    <dt>Campus</dt>
+                    <dd>Sao Paulo</dd>
+                    <dt>Course</dt>
+                    <dd>Web Development</dd>
+                </dl>
+                <div className="text-center">
+                    <button className="btn-logout">Logout</button>
+                </div>
+            </div>
+            <div className="col-right text-center">
+                <Title>IronProfile</Title>
+                <Text>Awesome to have at IronProfile again!</Text>
+
+                <button className="btn btn--secondary">Edit Photo</button>
+                <Small>The user is able to upload a new profile photo, using NodeJS and Multer uploader.</Small>
+            </div>
+        </>
+    )
+}
