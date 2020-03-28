9148e3501457afbd45b0056b1f8764c2b584294b
Carlos Mateo <cmc2k5@gmail.com>
2020-03-28 02:01:22 +0100

    API - Completed

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,17 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const hbs = require('hbs');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 mongoose
-    .connect('mongodb://localhost/ironprofile', { useNewUrlParser: true })
+    .connect('mongodb://localhost/ironprofile', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
     .then(x => {
         console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
     })
@@ -28,29 +31,20 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+    session({
+        secret: 'ironprofile-passport-authentication',
+        resave: true,
+        saveUninitialized: true,
+        store: new MongoStore({ mongooseConnection: mongoose.connection })
+    })
+);
 
-// Express View engine setup
+require('./passport')(app);
 
-app.use(require('node-sass-middleware')({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    sourceMap: true
-}));
-
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
-const index = require('./routes/index');
-app.use('/', index);
+// Routes
+const auth = require('./routes/auth');
+app.use('/auth', auth);
 
 
 module.exports = app;
