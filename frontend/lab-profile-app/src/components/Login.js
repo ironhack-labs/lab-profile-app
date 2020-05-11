96e0dad012f497ef82a5b270a81ab4c3aa230afa
donovanftl <donovan_torreslan@outlook.com>
2020-05-11 02:43:20 -0500

    no se conecta el back con el front

--- frontend/lab-profile-app/src/components/Login.js
+++ frontend/lab-profile-app/src/components/Login.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
-class Login extends Component {
-  state = {
-    username: '',
-    password: '',
-  };
-
-  handleInputChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    return (
+const index = ({handleInputs, login, username, password}) => {
+  return (
+    <div id="containerLogin">
       <div>
-        <h1>Login</h1>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={this.state.username}
-          onChange={this.handleInputChange}
-          placeholder="username"
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={this.state.password}
-          onChange={this.handleInputChange}
-          placeholder="password"
-        />
-        <br />
-        <button type="submit">LogIn</button>
+        <h1>Log In</h1>
+        <form onSubmit={login}>
+          <label>Username: </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleInputs}
+          />
+          <br />
+          <br />
+          <label>Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleInputs}
+          />
+          <br/>
+          <br/>
+          <button type="submit">LogIn</button>
+        </form>
+        <p>
+          If you don't have an account yet you can create your account
+          <Link to='/signup'> here</Link>
+        </p>
       </div>
-    );
-  }
-}
+      <div>
+        <h4>Hello!</h4>
+        <p>Awesome to have at IronProfile again!</p>
+        <small>
+          If you signup, you agree with all our terms and conditions where we
+          can do whatever we want whit the data!
+        </small>
+      </div>
+    </div>
+  );
+};
 
-export default Login;
+export default index;
--- frontend/lab-profile-app/src/components/Signup.js
+++ frontend/lab-profile-app/src/components/Signup.js
@@ -1,41 +1,67 @@
-import React, { Component } from 'react';
+import React from "react";
 
-class SignUp extends Component {
-  state = {
-    username: '',
-    password: '',
-  };
-
-  handleInputChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>SignUp</h1>
-        <input
-          type="text"
-          name="username"
-          id="username"
-          value={this.state.username}
-          onChange={this.handleInputChange}
-          placeholder="username"
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={this.state.password}
-          onChange={this.handleInputChange}
-          placeholder="password"
-        />
-        <br />
-        <input type="submit" value="Submit" />
+const Signup = ({
+  handleInput,
+  signup,
+  username,
+  password,
+  campus,
+  course
+}) => {
+  return (
+    <div id="containerSignUp">
+      <div id="inputs">
+        <h1>Sign Up</h1>
+        <form onSubmit={signup}>
+          <label>Username: </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleInput}
+          />
+          <br />
+          <br />
+          <label>Password: </label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleInput}
+          />
+          <br />
+          <br />
+          <label>Campus </label>
+          <input
+            type='text'
+            name='campus'
+            value={campus}
+            onChange={handleInput}
+          />
+          <br />
+          <br />
+          <label>Course </label>
+          <input
+            type='text'
+            name='course'
+            value={course}
+            onChange={handleInput}
+          />
+          <br />
+          <br />
+          <button type="submit">Create Account</button>
+        </form>
       </div>
-    );
-  }
-}
+      <div id="texto">
+        <h4>Hello!</h4>
+        <p>Welcome to IronProfile!</p>
+        <small>
+          If you signup, you agree with all our<br/> terms and conditions where we
+          can <br/>do whatever we want whit the data!
+        </small>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp;
+export default Signup;
