39068b2f86347ba4e84a09968cc8f070eca1f010
Alba Gustems <albagustems@gmail.com>
2020-08-05 15:12:15 +0200

    Iteration 3| Auth Service completed

--- auth-frontend/src/components/HomePage.js
+++ auth-frontend/src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export class HomePage extends Component {
+class HomePage extends Component {
   render() {
     return (
       <div className="window">
