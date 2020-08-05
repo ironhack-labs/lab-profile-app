7994759bc565b6c85e5fef7100fd10d3a4d3ab17
Alba Gustems <albagustems@gmail.com>
2020-08-05 14:06:06 +0200

    Iteration 1: The REST API completed and working (tested with Postman)

---
+++ auth-backend/app.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+// Requires for the CORS and auth
+const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
+require('./configs/passport');
+
+mongoose
+  .connect('mongodb://localhost/lab-auth-react', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error('Error connecting to mongo', err);
+  });
+
+const app_name = require('./package.json').name;
+const debug = require('debug')(
+  `${app_name}:${path.basename(__filename).split('.')[0]}`
+);
+
+const app = express();
+
+// Middleware Setup
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Express View engine setup
+
+app.use(
+  require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    sourceMap: true,
+  })
+);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+
+// Session Settings
+app.use(
+  session({
+    secret: 'some secret goes here',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport Use
+app.use(passport.initialize());
+app.use(passport.session());
+
+// default value for title local
+app.locals.title = 'Express - Generated with IronGenerator';
+
+// Enable CORS
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+);
+
+// Routes imports
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth-routes');
+
+app.use('/', index);
+app.use('/auth', authRoutes);
+
+module.exports = app;
