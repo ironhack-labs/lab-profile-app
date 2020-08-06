32bdca516d46ab4c6651035a1e4731e6e2d01f1a
David de Lamo <d.lamo.valverde@gmail.com>
2020-08-06 15:35:10 +0200

    iteration 2

--- frontend-lab-profile-app/src/App.js
+++ frontend-lab-profile-app/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Iron Profile</h1>
+      <h2>Today we will create an app with authoritation, adding some cool styles!</h2>
+      <Link className='btn' to='/signup'>Sign up</Link>
+      <Link className='btn' to='/login'>Log in</Link>
+      <Switch>
+        <Route path="/signup">
+          <h1>Sign up</h1>
+        </Route>
+        <Route path="/login">
+        <h1>Log in</h1> 
+        </Route>
+      </Switch>
     </div>
   );
 }
