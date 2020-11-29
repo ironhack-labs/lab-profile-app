0cdcfd384a4d42d8e9a532a16ed3a0cd2ddbf769
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-29 14:42:43 -0600

    styledd buttons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { Button } from 'antd';
+import { Divider } from 'antd';
 
-const Home = ({ history }) => {
+const Home = () => {
   return (
-    <div className="site-button-ghost-wrapper">
-      <h1>IronProfile</h1>
-      <Button type="primary" ghost>
-        Primary
-      </Button>
-      <Button ghost>Default</Button>
-      <Button type="dashed" ghost>
-        Dashed
-      </Button>
+    <div>
+      <div className="site-button-ghost-wrapper">
+        <h1>IronProfile</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne
+          merninisti licere mihi ista probare, quae sunt a te dicta? Refert
+          tamen, quo modo.
+        </p>
+        <Button type="primary" ghost>
+          Sign up
+        </Button>
+
+        <Divider />
+
+        <Button type="primary" ghost>
+          Login
+        </Button>
+      </div>
     </div>
   );
 };
